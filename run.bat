6c93c4f157f4d884e6990877e21f43df91659947
David Hallstrom <dhallstr@rams.colostate.edu>
2018-12-30 10:32:05 -0700

    Modified run scripts to prevent infinite recursion

--- run.bat
+++ run.bat
@@ -4,3 +4,6 @@ SET var=%%F
 )
 ECHO ..\halite3.exe --replay-directory ..\replays\ --no-logs %var%
 ..\halite3.exe --replay-directory ..\replays\ --no-logs %var%
+
+REM dangerous, delete this line if running other java processes.
+taskkill /f /im java.exe
