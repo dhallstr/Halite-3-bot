be60dea9f8414d38eee11b8742fd7c4a891b8c5c
David Hallstrom <dhallstr@rams.colostate.edu>
2019-01-13 16:00:04 -0700

    v97 improved dropoff placement and creation

--- dhallstr/Magic.java
+++ dhallstr/Magic.java
@@ -9,14 +9,10 @@ public class Magic {
     public static final String BOT_NAME = "Nix";
 
     // Dropoff constants
-    public static final int MIN_DIST_FOR_BUILD = 15;
+    public static final int MIN_DIST_FOR_BUILD = 9;
     public static final int BUILD_DROPOFF_RADIUS = 8;
-    public static final int MIN_HALITE_FOR_BUILD = 8000;
-    public static final int BUILD_BUFFER_HALITE = 0;
-    public static final int SHIPS_PER_DROPOFF = 13;
-    public static final int MIN_TURNS_LEFT_FOR_DROPOFF = 70;
-    public static final int DROPOFF_FRIENDLY_SHIP_RADIUS = 10;
-    public static final int MIN_FRIENDLY_AROUND_FOR_DROPOFF = 8;
+    public static final int MIN_SCORE_FOR_DROPOFF = 8000;
+    public static final int SHIPS_PER_DROPOFF = 15;
     public static int MAX_DROPOFFS = 1;// includes shipyard
 
 
@@ -65,7 +61,7 @@ public class Magic {
         int size = (width + height) / 2; // in case it is a rectangle
         END_GAME_HALITE += (int)((size - 32) / 32.0 * (45 - END_GAME_HALITE));
         if (isTwoPlayer) {
-            MAX_DROPOFFS = size / 8;
+            MAX_DROPOFFS = size / 6;
             SEARCH_DEPTH = 75;
             END_GAME_HALITE = 15;
 
@@ -81,7 +77,7 @@ public class Magic {
         else {
             END_GAME_DELIVER_HALITE = (int) (Constants.MAX_HALITE * 0.4);
             END_GAME_HALITE = 25;
-            MAX_DROPOFFS = size / 11;
+            MAX_DROPOFFS = size / 8;
             SEARCH_DEPTH = 60;
 
             // CLOP adjustments
